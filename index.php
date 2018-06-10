16dbd89a4d5f458e6ce4d8894fe5bcfb43c8dd57
ThiseJF <thisejf@gmail.com>
2018-06-10 17:47:57 +0200

    upgrade index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Mini Site - Catalogue</title>
+	<link rel="stylesheet" type="text/css" href="css/grid.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body class="container-mobile-1-tablette-desktop-12">
+	
+	<?php include("menu.php"); ?>
+
+	<div class="filAriane green container-mobile-1-tablette-desktop-12-tab-12-big-12">
+			<p class="-tab-12-big-12">VOUS ÊTES ICI: <a href="index.php">ACCUEIL</a></p>
+	</div>
+	<div class="slider container-mobile-1-tablette-desktop-12-tab-12-big-12"></div>
+	<main class="container-mobile-1-tablette-desktop-12-tab-12-big-12">
+		<section class="container-mobile-1-tablette-desktop-12-tab-12-big-9 pub">
+					<img class="-big-12" src="imgHomePage/gazzda.jpg" alt="gazzda new  collection">	
+				
+					<img src="imgHomePage/liam-fr.jpg" alt="nouveau modèle Liam">
+				
+					<img src="imgHomePage/vita-aluvia.jpg" alt="vita aluvia">
+				
+					<img src="imgHomePage/showroom-360.jpg" alt="showroom-360">
+			
+		</section>
+		<section class="container-mobile-1-tablette-desktop-12-tab-12-big-3 open">
+					<img class="-big-12" src="imgHomePage/i-shop-sundays_1.jpg" alt="sunday shop">
+					
+					<div class="horaire -big-12">
+						<p>Ouvert chaque 1er dimanche du mois (12h-18)</p>
+						<ul>
+							<li>3 juin</li>
+							<li>1 juilllet</li>
+							<li>5 août</li>
+							<li>2 spetembre</li>
+						</ul>
+						<p><a href="">+ lire la suite</a></p>
+					</div>
+		</section>
+		
+	</main>
+</body>
+</html>
--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
 <header class="container-mobile-1-tablette-desktop-12-tab-12-big-12">
-		<a href="index.html" class="logo -tab-6-big-6"><img src="content/logo.png" alt="logo"></a>
+		<a href="index.php" class="logo -tab-6-big-6"><img src="content/logo.png" alt="logo"></a>
 		<form name="search" action="#" method="POST" class="form container-tab-6-big-6">
 			<div>
 				<input type="search" name="search" value="" placeholder="Que recherchez-vous?" class="search" />
@@ -11,7 +11,7 @@
 	<hr class="-tab-12-big-12">
 	<nav class="menu container-mobile-1-tablette-desktop-12-tab-12-big-12">
 			<ul class="container-mobile-1-tablette-desktop-12-tab-6-big-12">
-				<li><a href="index.html">ACCUEIL</a></li>
+				<li><a href="index.php">ACCUEIL</a></li>
 				<li><a href="catalogue.php">MEUBLES</a></li>
 				<li><a href="#">LUMINAIRES</a></li>
 				<li><a href="#">ACCESSOIRES</a></li>
